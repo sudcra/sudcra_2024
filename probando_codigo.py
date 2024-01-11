125c781e209a45cb05b5233ae37902a3eece5d50
sudcra <141349634+sudcra@users.noreply.github.com>
2024-01-11 17:05:39 -0300

    m_01_11 - rutinas basicas de xlsx a df

--- probando_codigo.py
+++ probando_codigo.py
@@ -1,1 +1,6 @@
-print("Hola mundo")
+from xlsx_a_df import convertir_a_df_tipo_2
+
+ruta="C:\\Users\\mcorteze\\Desktop\\EMP1101-041V_VALPARAISO_E3.xlsm"
+
+df=convertir_a_df_tipo_2(ruta, "EMP1101-041V", 9, 52, 1, 3)
+print(df)
